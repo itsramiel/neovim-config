3944a1189ae4f040d370d4a6806ef4f4c2860388
ramiel <ramielwan48@gmail.com>
2022-12-31 17:08:26 +0200

    feat: configure eslint_d in a way where javascript projects without eslint will not get an error

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -1,10 +1,55 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local starts_with = function(str, start)
+  return string.sub(str, 1, #start) == start
+end
+
+local create_runtime_condition = function(config_names)
+  local bufnr_cache = {}
+  local config_path_cache = {}
+
+  return function(params)
+    if bufnr_cache[params.bufnr] ~= nil then
+      return bufnr_cache[params.bufnr]
+    else
+      for _, cached_config_path in ipairs(config_path_cache) do
+        if starts_with(params.bufname, cached_config_path) then
+          bufnr_cache[params.bufnr] = true
+          return true
+        end
+      end
+    end
+
+    local config_path =
+      require('lspconfig').util.root_pattern(config_names)(params.bufname)
+
+    local has_config = config_path ~= nil
+    if has_config then
+      table.insert(config_path_cache, config_path)
+    end
+    bufnr_cache[params.bufnr] = has_config
+
+    return has_config
+  end
+end
+
+local eslint_runtime_condition = create_runtime_condition({
+  '.eslintrc.cjs',
+  '.eslintrc.js',
+  '.eslintrc.json',
+  '.eslintrc.yaml',
+  '.eslintrc.yml',
+})
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d,
+    -- https://github.com/sQVe/dotfiles/blob/master/config/nvim/lua/sQVe/plugins/null-ls.lua
+    null_ls.builtins.diagnostics.eslint_d.with({
+      runtime_condition = eslint_runtime_condition,
+      timeout = 20000,
+    }),
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
