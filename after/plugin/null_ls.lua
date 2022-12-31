07dcef3c6bcd998cedccf4a6be77d719dcc70c1b
ramiel <ramielwan48@gmail.com>
2022-12-31 10:07:03 +0200

    feat: format null-ls

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -2,26 +2,26 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettierd,
-	null_ls.builtins.diagnostics.eslint_d,
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-		callback = function()
-		    vim.lsp.buf.format({
-        		bufnr = bufnr,
-			-- makes sure to use null-ls, not lsp or smthn else, for formatting
-		        filter = function(client)
-			    return client.name == "null-ls"
-			end
-		    })
-		end,
-            })
-        end
-    end,
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint_d,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+            -- makes sure to use null-ls, not lsp or smthn else, for formatting
+            filter = function(clnt)
+              return clnt.name == "null-ls"
+            end
+          })
+        end,
+      })
+    end
+  end,
 })
