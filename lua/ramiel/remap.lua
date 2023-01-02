e0466c8debb5f52010921a6bcf90c8740bcf1b41
ramiel <ramielwan48@gmail.com>
2023-01-02 09:29:05 +0200

    feat: go to next and previous buffer

--- lua/ramiel/remap.lua
+++ lua/ramiel/remap.lua
@@ -7,3 +7,7 @@ vim.keymap.set("n", "J", ":m .+1<CR>==")
 vim.keymap.set("n", "K", ":m .-2<CR>==")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- move to next and previous buffer
+vim.keymap.set("n", "<leader>x", ":bn<CR>")
+vim.keymap.set("n", "<leader>z", ":bp<CR>")
