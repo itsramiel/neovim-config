c2153abaf9c9319de9c01d9b544663991485503d
ramiel <ramielwan48@gmail.com>
2023-01-06 22:04:31 +0200

    refactor: define luasnip shorcuts using vim keymaps

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -7,15 +7,18 @@ ls.config.set_config {
 
 vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
 -- Yes, we're just executing a bunch of Vimscript using vim.cmd
-vim.cmd[[
-" Use Tab to expand and jump through snippets
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
-" Use Shift-Tab to jump backwards through snippets
-imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-]]
+vim.keymap.set({"i", "s"}, "<c-k>", function ()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<c-j>", function ()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, {silent = true})
 
 require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
 
