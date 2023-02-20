eda0fdd418f099a14a2b23ea413f7a3af418da1a
ramiel <ramielwan48@gmail.com>
2023-02-20 21:26:10 +0200

    feat: smart paths for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,11 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup {
+  defaults = {
+    path_display = { "smart" }
+  }
+}
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep)
 vim.keymap.set('n', '<leader>pe', builtin.diagnostics, {})
