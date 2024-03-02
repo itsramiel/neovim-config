ab9731199c34fa7a84294a38a35e333159e68121
Rami Elwan <ramielwan48@gmail.com>
2024-03-02 19:57:05 +0100

    feat: add snippets

--- lua/itsramiel/lazy.lua
+++ lua/itsramiel/lazy.lua
@@ -3,15 +3,19 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("itsramiel.plugins")
+require("lazy").setup("itsramiel.plugins", {
+	change_detection = {
+		notify = false,
+	},
+})
---
+++ lua/snippets/javascript.lua
@@ -0,0 +1,109 @@
+local ls = require("luasnip")
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+-- local d = ls.dynamic_node
+local c = ls.choice_node
+-- local r = ls.restore_node
+local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+-- local l = require("luasnip.extras").lambda
+
+local function firstToUpper(str)
+	return (str:gsub("^%l", string.upper))
+end
+
+local getBaseFilename = function(_, snip)
+	return snip.env.TM_FILENAME_BASE
+end
+
+return {
+	s(
+		"log",
+		fmt("console.log({}{}{});", {
+			c(1, { t('"'), t("'") }),
+			i(2),
+			rep(1),
+		})
+	),
+	s(
+		"clo",
+		fmt("console.log({}{}{},{});", {
+			c(1, { t('"'), t("'") }),
+			i(2),
+			rep(1),
+			rep(2),
+		})
+	),
+	s(
+		"useState",
+		fmt("const [{}, set{}] = useState{}({});", {
+			i(1),
+			f(function(args)
+				return firstToUpper(args[1][1])
+			end, { 1 }),
+			c(2, {
+				fmt("<{}>", i(1)),
+				t(""),
+			}),
+			i(3),
+		})
+	),
+	s(
+		"useRef",
+		fmt("const {} = useRef{}({});", {
+			i(1),
+			i(2),
+			i(3),
+		})
+	),
+	s(
+		"useEffect",
+		fmt(
+			[[
+  useEffect(() => {{
+    {}
+  }}, [])
+  ]],
+			{ i(0) }
+		)
+	),
+	s(
+		"f",
+		fmt(
+			[[
+    {}function {}({}){} {{
+      {}
+    }}
+    ]],
+			{
+				c(1, {
+					t("export "),
+					t(""),
+				}),
+				f(getBaseFilename),
+				i(2),
+				i(3),
+				i(0),
+			}
+		)
+	),
+	s(
+		"iaf",
+		fmt(
+			[[
+      ({}) => {{
+        {}
+      }}
+    ]],
+			{
+				i(1),
+				i(0),
+			}
+		)
+	),
+}
