e0e220b1c76da6fb51aeb05c4e01dac81d71b94f
Rami Elwan <ramielwan48@gmail.com>
2024-05-15 17:25:14 +0200

    feat: add treesitter playground

--- lua/itsramiel/plugins/treesitter.lua
+++ lua/itsramiel/plugins/treesitter.lua
@@ -1,45 +1,52 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-    },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/playground", -- Should no longer be needed with nvim 0.10+
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			-- import nvim-treesitter plugin
+			local treesitter = require("nvim-treesitter.configs")
 
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "yaml",
-          "html",
-          "css",
-          "markdown",
-          "markdown_inline",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "gitignore",
-          "swift",
-          "cpp",
-          "ruby"
-        },
-      })
-    end,
-  },
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+
+				-- playground, should be removed with neovim 0.10
+				playground = {
+					enable = true,
+				},
+
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
+					enable = true,
+				},
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"gitignore",
+					"swift",
+					"cpp",
+					"ruby",
+				},
+			})
+		end,
+	},
 }
