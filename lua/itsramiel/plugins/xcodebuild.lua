401c749e32f6a557cd380e679e2f33055166fb7b
Rami Elwan <ramielwan48@gmail.com>
2024-07-30 16:27:57 +0200

    feat: add xcode shortcuts

--- lua/itsramiel/plugins/xcodebuild.lua
+++ lua/itsramiel/plugins/xcodebuild.lua
@@ -10,5 +10,25 @@ return {
 		require("xcodebuild").setup({
 			-- put some options here or leave it empty to use default settings
 		})
+
+		local opts = { noremap = true, silent = true }
+
+		opts.desc = "Show Xcodebuild Actions"
+		vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", opts)
+
+		opts.desc = "Show Project Manager Actions"
+		vim.keymap.set("n", "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", opts)
+
+		opts.desc = "Build Project"
+		vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", opts)
+
+		opts.desc = "Build & Run Project"
+		vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", opts)
+
+		opts.desc = "Toggle Xcodebuild Logs"
+		vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", opts)
+
+		opts.desc = "Select Device"
+		vim.keymap.set("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", opts)
 	end,
 }
