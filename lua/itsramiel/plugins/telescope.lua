ab1f0e08046a33e8e8e14a6becb02606d94e13f1
Rami Elwan <ramielwan48@gmail.com>
2025-06-20 19:04:20 +0200

    feat: add mapping to restore deleted file in git status

--- lua/itsramiel/plugins/telescope.lua
+++ lua/itsramiel/plugins/telescope.lua
@@ -4,6 +4,11 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+		local utils = require("telescope.utils")
+		local action_state = require("telescope.actions.state")
+		local finders = require("telescope.finders")
+		local make_entry = require("telescope.make_entry")
+		local transform_mod = require("telescope.actions.mt").transform_mod
 
 		local function filename_first(_, path)
 			local tail = vim.fs.basename(path)
@@ -25,9 +30,83 @@ return {
 			end,
 		})
 
+		local restore_file_action = function(prompt_bufnr)
+			local picker = action_state.get_current_picker(prompt_bufnr)
+			local selection = action_state.get_selected_entry()
+			local cwd = picker.cwd
+			local status = selection.status
+
+			if status == " D" then
+				utils.get_os_command_output({ "git", "restore", selection.value }, cwd)
+			elseif status == "D " then
+				utils.get_os_command_output({ "git", "restore", "--staged", selection.value }, cwd)
+			end
+		end
+
+		local custom_actions = transform_mod({
+			restore_file = function(prompt_bufnr)
+				restore_file_action(prompt_bufnr)
+			end,
+		})
+
 		telescope.setup({
 			defaults = {
 				path_display = filename_first,
+			},
+			pickers = {
+				git_status = {
+					attach_mappings = function(prompt_bufnr, map)
+						custom_actions.restore_file:enhance({
+							post = function(prompt_bufnr)
+								local picker = action_state.get_current_picker(prompt_bufnr)
+								local current_row = picker:get_selection_row()
+								local current_index = picker:get_index(current_row)
+								local total_items = #picker.finder.results
+
+								local selection = action_state.get_selected_entry()
+								local file_name = selection.value
+
+								local callbacks = { unpack(picker._completion_callbacks) }
+								picker:register_completion_callback(function(self)
+									local new_picker = action_state.get_current_picker(prompt_bufnr)
+									local new_total_items = #new_picker.finder.results
+
+									local new_row
+									if total_items <= 1 then
+										new_row = nil
+									elseif new_total_items == total_items then
+										new_row = current_row
+									elseif current_index < total_items then
+										new_row = current_row
+									else
+										new_row = current_row + 1
+									end
+
+									if new_row then
+										self:set_selection(new_row)
+									end
+									self._completion_callbacks = callbacks
+								end)
+
+								-- rebuild finder
+								local args = { "status", "--porcelain=v1", "--", "." }
+								if vim.F.if_nil(picker.expand_dir, true) then
+									table.insert(args, #args - 1, "-uall")
+								end
+
+								local git_command = utils.__git_command
+
+								local git_cmd = git_command(args, picker)
+								local entry_maker = make_entry.gen_from_git_status(picker)
+								local new_finder = finders.new_oneshot_job(git_cmd, { entry_maker = entry_maker })
+
+								picker:refresh(new_finder, { reset_prompt = false })
+							end,
+						})
+						map("n", "rs", custom_actions.restore_file)
+						return true
+					end,
+				},
 			},
 		})
 
