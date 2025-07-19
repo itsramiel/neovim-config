00e68474c9f23dc3db1107760b3e153df4b4adad
Rami Elwan <ramielwan48@gmail.com>
2025-07-19 16:35:33 +0200

    chore: update treesitter

--- lua/itsramiel/plugins/treesitter.lua
+++ lua/itsramiel/plugins/treesitter.lua
@@ -1,50 +1,33 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
+		lazy = false,
+		branch = "main",
 		build = ":TSUpdate",
-		dependencies = {
-			"windwp/nvim-ts-autotag",
-			"nvim-treesitter/nvim-treesitter-context",
-		},
 		config = function()
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
+			local treesitter = require("nvim-treesitter")
 
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				matchup = {
-					enable = true,
-				},
-
-
-				highlight = {
-					enable = true,
-				},
-
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"markdown",
-					"markdown_inline",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"gitignore",
-					"swift",
-					"cpp",
-					"ruby",
-					"kotlin",
-					"cmake",
-					"java",
-				},
+			treesitter.install({
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"gitignore",
+				"swift",
+				"cpp",
+				"ruby",
+				"kotlin",
+				"cmake",
+				"java",
 			})
 		end,
 	},
