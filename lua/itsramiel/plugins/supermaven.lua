2746cbc019479dddf487732ab213e0ce40270e96
Rami Elwan <ramielwan48@gmail.com>
2025-09-27 09:29:28 +0200

    feat: toggle ai autocomplete

--- lua/itsramiel/plugins/supermaven.lua
+++ lua/itsramiel/plugins/supermaven.lua
@@ -1,5 +1,11 @@
 return {
 	"supermaven-inc/supermaven-nvim",
-	config = true,
 	event = { "BufReadPost" }, -- lazy load after reading a buffer
+	config = function()
+		require("supermaven-nvim").setup({})
+		local keymap = vim.keymap
+
+		local opts = { noremap = true, silent = true, desc = "Toggle supermaven" }
+		keymap.set("n", "<leader>it", "<cmd>SupermavenToggle<CR>", opts)
+	end,
 }
