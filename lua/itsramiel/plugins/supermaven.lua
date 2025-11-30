4ab2eeda8f22ef6c287aff53623f6196d556b1e2
Rami Elwan <ramielwan48@gmail.com>
2025-11-30 16:52:08 +0100

    refactor: disable supermaven by default

--- lua/itsramiel/plugins/supermaven.lua
+++ lua/itsramiel/plugins/supermaven.lua
@@ -2,7 +2,11 @@ return {
 	"supermaven-inc/supermaven-nvim",
 	event = { "BufReadPost" }, -- lazy load after reading a buffer
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			condition = function()
+				return true
+			end,
+		})
 		local keymap = vim.keymap
 
 		local opts = { noremap = true, silent = true, desc = "Toggle supermaven" }
