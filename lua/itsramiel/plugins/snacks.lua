dc185b5c9f8527b559648af8a8789ef516bfd84f
Rami Elwan <ramielwan48@gmail.com>
2025-11-30 16:53:38 +0100

    feat: add snacks

---
+++ lua/itsramiel/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+	dir = "/Users/itsramiel/Desktop/All/Lua/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		picker = { enabled = true },
+		image = {},
+		indent = {},
+		notifier = {},
+	},
+}
