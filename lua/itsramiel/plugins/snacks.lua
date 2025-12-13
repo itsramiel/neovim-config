1b8ad4ad105c169ad76803a14d1897ff2351c90f
Rami Elwan <ramielwan48@gmail.com>
2025-12-13 10:42:21 +0100

    feat: add snacks keybindings

--- lua/itsramiel/plugins/snacks.lua
+++ lua/itsramiel/plugins/snacks.lua
@@ -1,11 +1,28 @@
 return {
-	dir = "/Users/itsramiel/Desktop/All/Lua/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		picker = { enabled = true },
-		image = {},
-		indent = {},
-		notifier = {},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    picker = { enabled = true },
+    image = {},
+    indent = {},
+    notifier = {},
+    lazygit = {},
+  },
+  config = function()
+    local snacks = require("snacks")
+
+    local keymap = vim.keymap -- for conciseness
+    local opts = { noremap = true, silent = true }
+
+    opts.desc = "Display help list"
+    keymap.set("n", "<leader>lh", function()
+      snacks.picker.help()
+    end, opts)
+
+    opts.desc = "Lazy git"
+    keymap.set("n", "<leader>lg", function()
+      snacks.lazygit.open()
+    end, opts)
+  end,
 }
