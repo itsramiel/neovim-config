790df12c0f56073bb45bacbc1126b57ed956d362
Rami Elwan <ramielwan48@gmail.com>
2025-12-13 10:41:31 +0100

    feat: add type definitions for tests

--- lua/itsramiel/plugins/lazydev.lua
+++ lua/itsramiel/plugins/lazydev.lua
@@ -1,5 +1,15 @@
 return {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	config = true,
+  { "LuaCATS/busted",   name = "busted-types",   lazy = true },
+  { "LuaCATS/luassert", name = "luassert-types", lazy = true },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    config = true,
+    opts = {
+      library = {
+        { path = "busted-types/library",   words = { "describe" } },
+        { path = "luassert-types/library", words = { "assert" } },
+      },
+    },
+  },
 }
