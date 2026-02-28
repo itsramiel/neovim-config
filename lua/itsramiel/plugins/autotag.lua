2ef876b6f8093c27f90e7996d478f93ab76d9fc8
Rami Elwan <ramielwan48@gmail.com>
2026-02-28 17:01:12 +0100

    fix: disable auto tag closing in markdown

--- lua/itsramiel/plugins/autotag.lua
+++ lua/itsramiel/plugins/autotag.lua
@@ -1,5 +1,13 @@
 return {
-	"windwp/nvim-ts-autotag",
-	config = true,
-	event = { "BufReadPost" }, -- lazy load after reading a buffer
+  "windwp/nvim-ts-autotag",
+  event = { "BufReadPost" }, -- lazy load after reading a buffer
+  config = function()
+    require('nvim-ts-autotag').setup({
+      per_filetype = {
+        ["markdown"] = {
+          enable_close = false
+        }
+      }
+    })
+  end
 }
